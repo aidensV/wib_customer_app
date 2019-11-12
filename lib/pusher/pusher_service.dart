eb0bf94856cf774f6641e4f46d549168d89719a4
foxbloodiron <ariakbar6944@gmail.com>
2019-11-12 17:16:17 +0700

    Update pusher_service.dart

--- lib/pusher/pusher_service.dart
+++ lib/pusher/pusher_service.dart
@@ -6,22 +6,31 @@ import 'package:pusher_websocket_flutter/pusher.dart';
 import 'package:flutter/services.dart';
 import 'package:wib_customer_app/env.dart';
 import 'package:wib_customer_app/notification_service/notification_service.dart';
+import 'package:wib_customer_app/storage/storage.dart';
 
 class PusherService {
-  Event eventPenjualanOnline, eventPenjualanOffline;
+  Event eventPenjualanDiKonfirm, eventPenjualanDiProses, eventPenjulanDiKirim;
   String lastConnectionState;
+
   Channel penjualanChannelOn, penjualanChannelOff, penjualanChannel;
   NotificationService notificationService;
+
   PusherService({@required this.notificationService});
 
-  StreamController<String> eventDataPenjualanOnline =
+  StreamController<String> eventDataPenjualanDiKonfirm =
       StreamController<String>();
-  StreamController<String> eventDataPenjualanOffline =
+  StreamController<String> eventDataPenjualanDiProses =
       StreamController<String>();
-  Sink get _inEventData => eventDataPenjualanOnline.sink;
-  Sink get _inEventDataOff => eventDataPenjualanOffline.sink;
-  Stream get eventStream => eventDataPenjualanOnline.stream;
-  Stream get eventStreamOff => eventDataPenjualanOffline.stream;
+  StreamController<String> eventDataPenjualanDiKirim =
+      StreamController<String>();
+
+  Sink get _inEventDataDiKonfirm => eventDataPenjualanDiKonfirm.sink;
+  Sink get _inEventDataDiProses => eventDataPenjualanDiProses.sink;
+  Sink get _inEventDataDiKirim => eventDataPenjualanDiKirim.sink;
+
+  Stream get eventStreamDikonfirm => eventDataPenjualanDiKonfirm.stream;
+  Stream get eventStreamDiProses => eventDataPenjualanDiProses.stream;
+  Stream get eventStreamDiKirim => eventDataPenjualanDiKirim.stream;
 
   Future<void> initPusher() async {
     try {
@@ -43,7 +52,12 @@ class PusherService {
   Future<void> subscribePusher() async {
     // penjualanChannelOff = await Pusher.subscribe('penjualan-channel');
     // penjualanChannelOn = await Pusher.subscribe('penjualan-channel');
-    penjualanChannel = await Pusher.subscribe('penjualan-channel');
+    DataStore store = DataStore();
+
+    int userId = await store.getDataInteger('id');
+
+    penjualanChannel =
+        await Pusher.subscribe('penjualan-channel-${userId.toString()}');
   }
 
   void unSubscribePusher(String channelName) {
@@ -51,43 +65,59 @@ class PusherService {
   }
 
   bindEvent() {
-    penjualanChannel.bind('penjualan-payment-on', (last) {
-      eventPenjualanOnline = last;
+    penjualanChannel.bind('penjualan-di-konfirm', (last) {
+      eventPenjualanDiKonfirm = last;
 
-      eventDataPenjualanOnline.add(last.data);
+      eventDataPenjualanDiKonfirm.add(last.data);
 
       dynamic dataJson = jsonDecode(last.data);
       print(dataJson);
 
       notificationService.showBigTextNotification(
         // title: 'Segera melakukan proses packing',
-        title: 'Layanan Penjualan Online',
-        description:
-            'Customer ${dataJson['message']['customer']} dengan NO. NOTA ${dataJson['message']['nota']} sudah menyelesaikan proses pembayaran. Segera menyiapkan barang dan memproses NOTA tersebut.',
+        title: 'Transaksi Pembelian',
+        description: 'Nota ${dataJson['message']['nota']} telah di konfirm',
         payload: dataJson['message']['type'],
       );
     });
 
-    penjualanChannel.bind('penjualan-payment-off', (last) {
-      eventPenjualanOffline = last;
-      eventDataPenjualanOffline.add(last.data);
+    penjualanChannel.bind('penjualan-proses', (last) {
+      eventPenjualanDiProses = last;
+
+      eventDataPenjualanDiProses.add(last.data);
 
       dynamic dataJson = jsonDecode(last.data);
       print(dataJson);
 
       notificationService.showBigTextNotification(
         // title: 'Segera melakukan proses packing',
-        title: 'Layanan Penjualan Offline',
-        description:
-            'Customer ${dataJson['message']['customer']} dengan NO. NOTA ${dataJson['message']['nota']} sudah menyelesaikan proses pembayaran. Segera menyiapkan barang dan memproses NOTA tersebut.',
+        title: 'Transaksi Pembelian',
+        description: 'Nota ${dataJson['message']['nota']} telah di proses',
+        payload: dataJson['message']['type'],
+      );
+    });
+
+    penjualanChannel.bind('penjualan-delivered', (last) {
+      eventPenjulanDiKirim = last;
+
+      eventDataPenjualanDiKirim.add(last.data);
+
+      dynamic dataJson = jsonDecode(last.data);
+      print(dataJson);
+
+      notificationService.showBigTextNotification(
+        // title: 'Segera melakukan proses packing',
+        title: 'Transaksi Pembelian',
+        description: 'Nota ${dataJson['message']['nota']} telah di kirim dengan no. ekspedisi ${dataJson['message']['eskpedisi']}',
         payload: dataJson['message']['type'],
       );
     });
   }
 
   void unbindEvent() {
-    penjualanChannel.unbind('penjualan-payment-on');
-    penjualanChannel.unbind('penjualan-payment-off');
+    penjualanChannel.unbind('penjualan-di-konfirm');
+    penjualanChannel.unbind('penjualan-delivered');
+    penjualanChannel.unbind('penjualan-proses');
     // penjualanChannelOn.unbind('penjualan-payment-on');
     // penjualanChannelOn.unbind('penjualan-payment-off');
     // _eventData.close();
