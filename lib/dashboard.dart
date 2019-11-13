6a22982cb45588060d6b0e4e2ca732649c0fcf5b
Bakhrul <Bakhrulrpl@gmail.com>
2019-11-13 09:18:53 +0700

    push

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -16,6 +16,8 @@ import 'dart:convert';
 import 'package:wib_customer_app/pages/shops/category_item.dart';
 import 'package:flutter_pagewise/flutter_pagewise.dart';
 import 'pages/shops/product_detail.dart';
+import 'pages/wishlist/wishlist.dart';
+import 'pages/shopping_cart/shoppingcart.dart';
 
 String tokenType, accessToken;
 Map<String, String> requestHeaders = Map();
@@ -657,14 +659,22 @@ class _DashboardPageState extends State<DashboardPage>
                         actions: <Widget>[
                           IconButton(
                             onPressed: () {
-                              MyNavigator.goWishlist(context);
+                              Navigator.push(
+                              context,
+                              MaterialPageRoute(settings: RouteSettings(name: '/wishlist'),
+                                builder: (context) => Wishlist(),
+                              ));
                             },
                             icon: Icon(Icons.favorite),
                             // color: Colors.white,
                           ),
                           IconButton(
                             onPressed: () {
-                              MyNavigator.goKeranjang(context);
+                              Navigator.push(
+                              context,
+                              MaterialPageRoute(settings: RouteSettings(name: '/keranjangbelanja'),
+                                builder: (context) => Keranjang(),
+                              ));
                             },
                             icon: Icon(Icons.shopping_cart),
                             // color: Colors.white,
