5149118303cb20d4633e7935354aa7033c342135
foxbloodiron <ariakbar6944@gmail.com>
2019-12-25 09:48:37 +0700

    Update env.dart

--- lib/env.dart
+++ lib/env.dart
@@ -23,16 +23,16 @@
 String clientId = '2';
 String grantType = 'password';
 
-// String appId = '859057';
-// String key = "aaf58bdb288796ca641a";
-// String secret = "c4ab4e43e9c599c3852d";
-// String cluster = "ap1";
+String appId = '859057';
+String key = "aaf58bdb288796ca641a";
+String secret = "c4ab4e43e9c599c3852d";
+String cluster = "ap1";
 
 // === Pusher Ari (xyzab-development) ===
-String appId = "686477";
-String key = "f3dfb944b5caa13e1438";
-String secret = "60a602df4ca57cb4dd9b";
-String cluster = "ap1";
+// String appId = "686477";
+// String key = "f3dfb944b5caa13e1438";
+// String secret = "60a602df4ca57cb4dd9b";
+// String cluster = "ap1";
 
 url(pathname){
   var path = pathname;  
